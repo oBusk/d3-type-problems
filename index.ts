1350bef88409ee3c8a9df7a156739b1243afed49
Oscar Busk <oscar.busk@gmail.com>
2023-04-07 01:02:07 +0200

    Small update

--- index.ts
+++ index.ts
@@ -4,13 +4,28 @@ import { Transition } from "d3-transition";
 
 const selection = select("body");
 
+// using a selection works
 const updatedSelection = selection.attr("foo", "bar").style("color", "red");
 
 const transition = selection.transition();
 
+// using a transition works
 const updatedTransition = transition.attr("foo", "bar").style("color", "red");
 
-function testType(
+// In any scenario where they can return either a selection or a transition,
+// it will fail.
+function usingUnion(
+  selectionOrTransition:
+    | Selection<BaseType, any, BaseType, any>
+    | Transition<BaseType, any, BaseType, any>
+) {
+  return selectionOrTransition
+    .attr("foo", "bar")
+    .style("color", "red")
+    .selection();
+}
+
+function selOrTra(
   selectionOrTransition: SelectionOrTransition<BaseType, any, BaseType, any>
 ) {
   return selectionOrTransition
